2ce4d7991af0fec4205ea24acb44d9686526173b
Scott Griffin <mscottgriffin@gmail.com>
2020-08-11 11:22:09 -0400

    welcome page rcc scaffold

---
+++ client/src/containers/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./Welcome.css";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Welcome;
