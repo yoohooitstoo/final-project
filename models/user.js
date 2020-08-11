c9f8f0caf46e37f9a383eee590ddc78cf4671f4a
Jared Porter <jaredp17@me.com>
2020-08-11 11:40:21 -0400

    Added renting field to hold data of books a user is currently renting.

--- models/user.js
+++ models/user.js
@@ -3,51 +3,55 @@ const { bookSchema } = require("./book");
 const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
-
-const userSchema =  new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 1
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  zipCode: {
+    type: Number,
+    required: true,
+    minlength: 1,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  ownedBooks: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Book",
     },
-    username: {
-        type: String,
-        required: true, 
-        minlength: 1
+  ],
+  renting: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Book",
     },
-    email: {
-        type: String,
-        required: true,
-        minlength: 1
-    },
-    zipCode: {
-        type: Number,
-        required: true,
-        minlength: 1
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 1
-    },
-    ownedBooks: [
-        {
-        type: Schema.Types.ObjectId,
-        ref: "Book"
-        }
-    ]
-})
-
+  ],
+});
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign(
-        { _id: this._id, username: this.username },
-        "jwt=myPRIVATEKEY"
-        // process.env.JWT_PRIVATEKEY - add when deployed
-    )
-    return token;
-}
+  const token = jwt.sign(
+    { _id: this._id, username: this.username },
+    "jwt=myPRIVATEKEY"
+    // process.env.JWT_PRIVATEKEY - add when deployed
+  );
+  return token;
+};
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
 module.exports.User = User;
