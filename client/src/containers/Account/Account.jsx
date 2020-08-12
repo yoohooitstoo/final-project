ee0e2ec1f56b32a64cdcda13aa5ca6951ac0261e
yoohooitstoo <stumanscott@gmail.com>
2020-08-12 16:05:43 -0400

    the account layout is set up

--- client/src/containers/Account/Account.jsx
+++ client/src/containers/Account/Account.jsx
@@ -5,11 +5,72 @@ class Account extends Component {
   render() {
     return (
       <div className="container is-fluid">
-        <div className="notification">This is the users account.</div>
-        <div className="buttons">
-          <Link to="/">
-            <button className="button is-primary">Home</button>
-          </Link>
+        <div className="notification has-background-warning-dark">
+          <div className="buttons is-right">
+            <Link to="/">
+              <button className="button is-primary">
+                Log Out
+              </button>
+            </Link>
+          </div>
+          <div className="tile is-ancestor">
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Your Library</p>
+              </div>
+            </div>
+          </div>
+          <div className="tile is-ancestor">
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+          </div>
+          <div className="tile is-ancestor">
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Borrowed Library</p>
+              </div>
+            </div>
+          </div>
+          <div className="tile is-ancestor">
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+            <div className="tile is-parent">
+              <div className="tile is-child box">
+                <p className="title has-text-centered">Book</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
