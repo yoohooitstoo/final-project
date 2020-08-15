c4f828d31f13c419b832ee39533fd3f20e545779
Scott Griffin <mscottgriffin@gmail.com>
2020-08-15 14:32:19 -0400

    hover button working

--- client/src/containers/Main/Main.jsx
+++ client/src/containers/Main/Main.jsx
@@ -1,69 +1,77 @@
 import React, { Component } from 'react';
 import './Main.css';
+import Navbar from "../../components/Navbar/Navbar"
+import BookInfo from '../../components/BookInfo/BookInfo';
 // import { Link } from 'react-router-dom';
 
 class Main extends Component {
   render() {
     return (
       // Navbar component
-      <div className="container">
-        <div className="columns bookrow">
-          <div class="column"></div>
-          <div class="column">
-            {/* //Book Cover from database */}
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            {/* //Book title from database */}
-            <p class="title">One</p>
+      <div>
+        < Navbar />
+        <div classNameName="container">
+          <div className="columns bookrow">
+            <div className="column"></div>
+            <div className="column">
+              {/* //Book Cover from database */}
+              <div className="figure is-320x480 bookcover">
+                <img src="https://placekitten.com/320/480"/>
+                <div className= "middle">
+                  <button className="button is-link" href ={BookInfo}>Click</button>
+                </div>
+              </div>
+              {/* //Book title from database */}
+              <p className="title">One</p>
+            </div>
+            <div className="column">
+              <figure className="image is-320x480">
+                <img src="https://placekitten.com/320/480" />
+              </figure>
+              <p className="title">Two</p>
+            </div>
+            <div className="column">
+              <figure className="image is-320x480">
+                <img src="https://placekitten.com/320/480" />
+              </figure>
+              <p className="title">Three</p>
+            </div>
+            <div className="column">
+              <figure className="image is-320x480">
+                <img src="https://placekitten.com/320/480" />
+              </figure>
+              <p className="title">Four</p>
+            </div>
+            <div className="column"></div>
           </div>
-          <div class="column">
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            <p class="title">Two</p>
+          <div className="columns bookrow">
+            <div className="column"></div>
+            <div className="column">
+              <figure className="image is-320x480">
+                <img src="https://placekitten.com/320/480" />
+              </figure>
+              <p className="title">Five</p>
+            </div>
+            <div className="column">
+              <figure className="image is-320x480">
+                <img src="https://placekitten.com/320/480" />
+              </figure>
+              <p className="title">Six</p>
+            </div>
+            <div className="column">
+              <figure className="image is-320x480">
+                <img src="https://placekitten.com/320/480" />
+              </figure>
+              <p className="title">Seven</p>
+            </div>
+            <div className="column">
+              <figure className="image is-320x480">
+                <img src="https://placekitten.com/320/480" />
+              </figure>
+              <p className="title">Eight</p>
+            </div>
+            <div className="column"></div>
           </div>
-          <div class="column">
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            <p class="title">Three</p>
-          </div>
-          <div class="column">
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            <p class="title">Four</p>
-          </div>
-          <div class="column"></div>
-        </div>
-        <div className="columns bookrow">
-          <div class="column"></div>
-          <div class="column">
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            <p class="title">Five</p>
-          </div>
-          <div class="column">
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            <p class="title">Six</p>
-          </div>
-          <div class="column">
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            <p class="title">Seven</p>
-          </div>
-          <div class="column">
-            <figure class="image is-320x480">
-              <img src="https://placekitten.com/320/480" />
-            </figure>
-            <p class="title">Eight</p>
-          </div>
-          <div class="column"></div>
         </div>
       </div>
     );
