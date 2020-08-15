5d15dbabdfcd147e5d53f90226033dd15a0aadc9
Jared Porter <jaredp17@me.com>
2020-08-15 01:33:05 -0400

    Routes refactoring.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
 const userRoutes = require("./users");
+const authRoutes = require("./authenticate")
 
 // Book routes
 router.use("/books", bookRoutes);
 
 // User routes
 router.use("/users", userRoutes);
+
+// Authentication routes
+router.use("/login", authRoutes);
 
 module.exports = router;
