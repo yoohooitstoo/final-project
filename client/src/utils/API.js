47486b688bc8ffb57ab61c9f98dc73f16f04c73f
Jared Porter <jaredp17@me.com>
2020-08-17 11:18:31 -0400

    Changed routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,32 +1,31 @@
 import axios from "axios";
-const PORT = process.env.PORT + 1 || 3000 || 3001;
 
 export default {
     getOwnedBooks: function(id) {
         console.log(id)
-        return axios.get(`http://localhost:${PORT}/api/users/${id}`)
+        return axios.get(`/api/users/${id}`)
     },
     addOwnedBook: function(id, book) {
         console.log(id)
-        return axios.put(`http://localhost:${PORT}/api/users/added/${id}`, book)
+        return axios.put(`/api/users/added/${id}`, book)
         //may have to change to 3001 to avoid cors policy!!!
     },
     deleteOwnedBook: function(id) {
         console.log(id)
-        return axios.delete(`http://localhost:${PORT}/api/books/${id}`)
+        return axios.delete(`/api/books/${id}`)
     },
     requestToRent: function(id, user) {
         console.log(id)
         console.log(user)
-        return axios.put(`http://localhost:${PORT}/api/books/${id}`, {userId: user});
+        return axios.put(`/api/books/${id}`, {userId: user});
     },
     getAllOwnedBooks: function() {
         console.log("hello")
-        return axios.get(`http://localhost:${PORT}/api/books`)
+        return axios.get(`/api/books`)
     },
     bookRented: function(id, book) {
         console.log(id)
         console.log(book)
-        return axios.put(`http://localhost:${PORT}/api/users/rent/${id}`, {bookId: book})
+        return axios.put(`/api/users/rent/${id}`, {bookId: book})
     }
 }
